d34927b8ea2531f4d6e3cf0a8589ee940acf7524
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-02-18 12:50:21 -0500

    l7/2 navbar w/routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HabitComponent } from './components/habit/habit.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'habits', component: HabitComponent },
+    { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent},
+    { path: '', component: HomeComponent}
+];
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HabitComponent } from './app/components/habit/habit.component';
+import { HomeComponent } from './app/components/home/home.component';
 
-bootstrapApplication(HabitComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
